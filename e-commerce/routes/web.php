02457e11e6fc69de41096383a81b7801c487dca3
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-13 17:43:20 +0200

    Shop, Cart And Checkout Page Added

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', \App\Http\Livewire\HomeComponent::class);
+Route::get('/shop', \App\Http\Livewire\ShopComponent::class);
+Route::get('/cart', \App\Http\Livewire\CartComponent::class);
+Route::get('/checkout', \App\Http\Livewire\CheckoutComponent::class);
