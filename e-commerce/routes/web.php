41b37558a48013e5fe3c5a0f601f89becae4b9b9
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-13 20:23:37 +0200

    Admin And User Authentication Added

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -21,3 +21,17 @@ Route::get('/', \App\Http\Livewire\HomeComponent::class);
 Route::get('/shop', \App\Http\Livewire\ShopComponent::class);
 Route::get('/cart', \App\Http\Livewire\CartComponent::class);
 Route::get('/checkout', \App\Http\Livewire\CheckoutComponent::class);
+
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//    return view('dashboard');
+//})->name('dashboard');
+
+// User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/user/dashboard', \App\Http\Livewire\User\UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
+    Route::get('/admin/dashboard', \App\Http\Livewire\Admin\AdminDashboardComponent::class)->name('admin.dashboard');
+});
