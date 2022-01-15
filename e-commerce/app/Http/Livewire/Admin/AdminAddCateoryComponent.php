87c5a9f0649878f42a93eae9af7808cdefaac715
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-15 17:13:08 +0200

    Admin Add Category Added

--- e-commerce/app/Http/Livewire/Admin/AdminAddCateoryComponent.php
+++ e-commerce/app/Http/Livewire/Admin/AdminAddCateoryComponent.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class AdminAddCateoryComponent extends Component
 {
+    public $name;
+    public $slug;
+
+    public function generateslug() {
+        $this->slug = Str::slug($this->name);
+    }
+
+    public function storeCategory() {
+        $category = new Category();
+        $category->name = $this->name;
+        $category->slug = $this->slug;
+        $category->save();
+        session()->flash('message', 'Category has been created successfully');
+    }
+
     public function render()
     {
-        return view('livewire.admin.admin-add-cateory-component');
+        return view('livewire.admin.admin-add-cateory-component')->layout('layouts.base');
     }
 }
