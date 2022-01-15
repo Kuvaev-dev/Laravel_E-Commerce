ee03d99d9d48a96e304cca74d9cbd1876ba88c3e
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-15 16:33:59 +0200

    Admin Check Categories Component Added

---
+++ e-commerce/app/Http/Livewire/Admin/AdminAddCateoryComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminAddCateoryComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-add-cateory-component');
+    }
+}
--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -38,4 +38,5 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
     Route::get('/admin/dashboard', \App\Http\Livewire\Admin\AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', \App\Http\Livewire\Admin\AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add', \App\Http\Livewire\Admin\AdminAddCateoryComponent::class)->name('admin.addcategory');
 });
