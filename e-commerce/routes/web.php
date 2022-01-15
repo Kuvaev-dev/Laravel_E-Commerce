f01911de318433c959ef791d61490d3e812c3fae
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-15 19:13:57 +0200

    Admin Product Page Added

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -40,4 +40,5 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
     Route::get('/admin/categories', \App\Http\Livewire\Admin\AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add', \App\Http\Livewire\Admin\AdminAddCateoryComponent::class)->name('admin.addcategory');
     Route::get('/admin/category/edit/{category_slug}', \App\Http\Livewire\Admin\AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/products', \App\Http\Livewire\Admin\AdminProductComponent::class)->name('admin.products');
 });
