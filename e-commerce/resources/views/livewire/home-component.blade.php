7820a1e0e50e1e6d17f761abfcd6236176024a6f
Kuvaev-dev <likeaboss13371@gmail.com>
2022-02-09 18:58:46 +0200

    Shop Links Added # 2

--- e-commerce/resources/views/livewire/home-component.blade.php
+++ e-commerce/resources/views/livewire/home-component.blade.php
@@ -89,7 +89,7 @@
         <div class="wrap-show-advance-info-box style-1">
             <h3 class="title-box">Popular Products</h3>
             <div class="wrap-top-banner">
-                <a href="#" class="link-banner banner-effect-2">
+                <a href="/shop" class="link-banner banner-effect-2">
                     <figure><img src="{{ asset('assets/images/digital-electronic-banner.jpg') }}" width="1170" height="240" alt=""></figure>
                 </a>
             </div>
@@ -122,7 +122,7 @@
         <div class="wrap-show-advance-info-box style-1">
             <h3 class="title-box">Product Categories</h3>
             <div class="wrap-top-banner">
-                <a href="#" class="link-banner banner-effect-2">
+                <a href="/shop" class="link-banner banner-effect-2">
                     <figure><img src="{{ asset('assets/images/fashion-accesories-banner.jpg') }}" width="1170" height="240" alt=""></figure>
                 </a>
             </div>
